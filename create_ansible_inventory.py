25284cbe6facbec08e6ab0674954f2c18d0f7f85
denisami <denis.sanchez.amieva@gmail.com>
2021-04-10 01:02:20 +0200

    add changes

--- create_ansible_inventory.py
+++ create_ansible_inventory.py
@@ -1,8 +1,10 @@
 def create_inventory(inventory):
     result = '---\n'
+    identation= ''
     for group in inventory:
         if group.get("name") == "all":
             result += "all:\n"
+            identation= '    '
             if "hosts" in group:
                 result += "  hosts:\n"
                 for host in group.get("hosts"):
@@ -13,20 +15,20 @@ def create_inventory(inventory):
                     result += "    " + key + ":" + value + "\n"
             result += "  children:\n"
         else:
-            result += "    " + group.get("name") + ":" + "\n"
-            result += "      hosts:\n"
+            result += identation + group.get("name") + ":" + "\n"
+            result += identation + "  hosts:\n"
             for host in group.get("hosts"):
-                result += "        " + host + ":" + "\n"
+                result += identation + "    " + host + ":" + "\n"
             if "vars" in group:
-                result += "      vars:\n"
+                result += identation + "  vars:\n"
                 for key, value in group.get("vars").items():
-                    result += "        " + key + ":" + value + "\n"
+                    result += identation + "    " + key + ":" + value + "\n"
     return result
 
 
 
 
-'''inventory = [
+inventory = [
     {
         "name": "all",
         "hosts": ["120.0.0.1", "192.168.1.195"],
@@ -50,4 +52,21 @@ def create_inventory(inventory):
     }
 ]
 
-print(create_inventory(inventory))'''
+inventory_2 = [
+    {
+        "name": "web_servers",
+        "hosts": ["10.23.0.12", "192.168.1.195"],
+        "vars": {
+            "ssh_root_pass": "{{ root_pass }}"
+        }
+    },
+    {
+        "name": "db_servers",
+        "hosts": ["10.23.0.18", "192.168.1.89"],
+        "vars": {
+            "ssh_root_user": "{{ root_user }}"
+        }
+    }
+]
+
+print(create_inventory(inventory_2))
