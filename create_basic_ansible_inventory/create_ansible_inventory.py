33aabf1a9f1676adfcae21f72b72b1e91d5451c1
denis.sanchez <denissanchez@sprinter.es>
2021-04-13 17:05:34 +0200

    add changes

--- create_basic_ansible_inventory/create_ansible_inventory.py
+++ create_basic_ansible_inventory/create_ansible_inventory.py
@@ -15,13 +15,13 @@ def create_basic_inventory(*args):
     templateLoader = jinja2.FileSystemLoader(searchpath="./")
     templateEnv = jinja2.Environment(loader=templateLoader)
     templateEnv.filters['jsonify'] = json.dumps
-    template = templateEnv.get_template('inventory_template.yaml')
+    template = templateEnv.get_template('inventory_template.yml')
     page = {
         'hosts': hosts,
         'vars': vars
     }
 # Mapping key:value page vble to template and writing in a file
-    f = open("inventory.yaml", "w")
+    f = open("inventory.yml", "w")
     f.write(template.render(page=page))
     f.close()
 
