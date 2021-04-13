386f51086db9155d33145c3aac5859a6c02cb2a9
denisami <denis.sanchez.amieva@gmail.com>
2021-04-13 16:33:47 +0200

    add changes

---
+++ create_basic_ansible_inventory/create_ansible_inventory.py
@@ -0,0 +1,27 @@
+import sys, json, jinja2
+
+def create_basic_inventory(*args):
+# Extract hosts:
+    hosts = args[0].split("=")[1].split(",")
+    print(hosts)
+
+# Extract vars:
+    vars={}
+    for arg in args[1:]:
+        vars[arg.split("=")[0]] = arg.split("=")[1]
+    print(vars)
+
+# Creating jinja Environment
+    templateLoader = jinja2.FileSystemLoader(searchpath="./")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    templateEnv.filters['jsonify'] = json.dumps
+    template = templateEnv.get_template('inventory_template.yaml')
+    page = {
+        'hosts': hosts,
+        'vars': vars
+    }
+    # Mapping key:value page vble to template and writing in a file
+    print(template.render(page=page))
+
+if __name__ == "__main__":
+    create_basic_inventory(*sys.argv[1:])
